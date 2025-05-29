3856cab0228b20719bdb1ef51b751e6c4db295f2
VictorWanjala <wanjala94vic@gmail.com>
2025-05-29 16:36:50 +0300

    disabled eslint for covertBigint

--- app/api/_utils/converBigInt.ts
+++ app/api/_utils/converBigInt.ts
@@ -1,42 +1,9 @@
-type JSONValue =
-  | string
-  | number
-  | boolean
-  | null
-  | Date
-  | bigint
-  | JSONObject
-  | JSONArray;
+/* eslint-disable @typescript-eslint/no-explicit-any */
 
-interface JSONObject {
-  [key: string]: JSONValue;
-}
-
-interface JSONArray extends Array<JSONValue> {}
-
-export function convertBigIntToString<T extends JSONValue>(obj: T): T | string | JSONObject | JSONArray {
-  if (obj === null || obj === undefined) return obj;
-
-  if (typeof obj === "bigint") {
-    return obj.toString();
-  }
-
-  if (obj instanceof Date) {
-    return obj.toISOString();
-  }
-
-  if (typeof obj === "object") {
-    if (Array.isArray(obj)) {
-      return obj.map(convertBigIntToString);
-    }
-
-    const result: JSONObject = {};
-    for (const key in obj as JSONObject) {
-      if (key === "password") continue;
-      result[key] = convertBigIntToString((obj as JSONObject)[key]);
-    }
-    return result;
-  }
-
-  return obj;
+export default function convertBigintToString(obj: any): any {
+  return JSON.parse(
+    JSON.stringify(obj, (_key, value) =>
+      typeof value === "bigint" ? value.toString() : value
+    )
+  );
 }
